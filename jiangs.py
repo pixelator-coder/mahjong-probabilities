cf3faf4e3d2243479b42400ed673d7690e3d7986
pixelator-coder <imaginary@fake.com>
2026-05-18 21:41:30 -0400

    Extract the logic for is_jiang into a method and make it work for any hand size (HOPEFULLY!!!)

--- jiangs.py
+++ jiangs.py
@@ -1,32 +1,39 @@
 import random
 import math
 
-tile_pool = ["1-wan1", "1-wan2", "1-wan3", "1-wan4",
-             "2-wan1", "2-wan2", "2-wan3", "2-wan4",
-             "3-wan1", "3-wan2", "3-wan3", "3-wan4",
-             "4-wan1", "4-wan2", "4-wan3", "4-wan4",
-             "5-wan1", "5-wan2", "5-wan3", "5-wan4",
-             "6-wan1", "6-wan2", "6-wan3", "6-wan4",
-             "7-wan1", "7-wan2", "7-wan3", "7-wan4",
-             "8-wan1", "8-wan2", "8-wan3", "8-wan4",
-             "9-wan1", "9-wan2", "9-wan3", "9-wan4",]
-hand = set()
+tile_pool = ["1-wan", "1-wan", "1-wan", "1-wan",
+             "2-wan", "2-wan", "2-wan", "2-wan",
+             "3-wan", "3-wan", "3-wan", "3-wan",
+             "4-wan", "4-wan", "4-wan", "4-wan",
+             "5-wan", "5-wan", "5-wan", "5-wan",
+             "6-wan", "6-wan", "6-wan", "6-wan",
+             "7-wan", "7-wan", "7-wan", "7-wan",
+             "8-wan", "8-wan", "8-wan", "8-wan",
+             "9-wan", "9-wan", "9-wan", "9-wan",]
+hand = []
 num_is_jiang = 0
-num_hands = 0
+num_possible_hands = 0
+
+def is_jiang(hand):
+    num_2s = hand.count("2-wan")
+    num_5s = hand.count("5-wan")
+    num_8s = hand.count("8-wan")
+    if num_2s >= 2 or num_5s >= 2 or num_8s >= 2:
+        return True
+    return False
 
 for first_tile in tile_pool:
+    hand.clear()
     new_tile_pool = tile_pool.copy()
-    hand.add(first_tile)
+    hand.append(first_tile)
     new_tile_pool.remove(first_tile)
     for second_tile in new_tile_pool:
-        hand.add(second_tile)
-        if "2-wan" in first_tile and "2-wan" in second_tile:
+        hand.append(second_tile)
+        if is_jiang(hand):
             num_is_jiang += 1
-        if "5-wan" in first_tile and "5-wan" in second_tile:
-            num_is_jiang += 1
-        if "8-wan" in first_tile and "8-wan" in second_tile:
-            num_is_jiang += 1
-        num_hands += 1
-print(num_hands)
+        num_possible_hands += 1
+        hand.pop(1)
+
+print(num_possible_hands)
 print(num_is_jiang)
-print(str(100*num_is_jiang/num_hands) + " percent.")
+print(str(100*num_is_jiang/num_possible_hands) + " percent.")
